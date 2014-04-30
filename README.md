430afd987666d21bd68f4d777de88e28d87e4875
Luca Mussi <ing.luca.mussi@gmail.com>
2014-04-30 02:43:50 +0200

    added stub from accounts-templates-bootstrap

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+[![Build Status](https://travis-ci.org/splendido/accounts-templates-foundation.svg?branch=master)](https://travis-ci.org/splendido/accounts-templates-foundation)
 accounts-templates-foundation
 =============================
 
 Meteor sign up and sign in templates styled for Zurb Foundation
+
+This package depends on [accounts-templates-core](https://github.com/splendido/accounts-templates-core.git)
+
+Learn more at [here](http://accounts-templates.meteor.com)
---
+++ package.js
@@ -0,0 +1,49 @@
+Package.describe({
+    summary: "Accounts Templates styled for Zurb Foundation."
+});
+
+Package.on_use(function(api, where) {
+    api.use([
+        'service-configuration',
+        'accounts-base',
+        'accounts-templates-core',
+        'accounts-merge',
+        'templating',
+        'less'
+    ], 'client');
+
+    api.add_files([
+        'lib/accounts-templates-foundation.html',
+        'lib/accounts-templates-foundation.js',
+        'lib/accounts-templates-foundation.less'
+    ], ['client']);
+
+    /*
+    api.use([
+        'service-configuration',
+        'accounts-base',
+    ], ['client', 'server']);
+    */
+
+    api.use([
+        'service-configuration',
+        'accounts-password',
+        'accounts-base',
+        'accounts-templates-core',
+    ], 'server');
+
+    api.imply([
+        'accounts-base',
+        'accounts-templates-core',
+        'service-configuration',
+    ], ['client', 'server']);
+});
+
+Package.on_test(function(api) {
+    api.use([
+        'accounts-templates-foundation',
+        'accounts-templates-core',
+    ]);
+    api.use(['tinytest', 'test-helpers'], ['client', 'server']);
+    api.add_files('tests/accounts-templates-foundation_tests.js', ['client', 'server']);
+});
