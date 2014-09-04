2e9a6ec4ed593b348ed5e5235dfb22b374cf91aa
Luca Mussi <ing.luca.mussi@gmail.com>
2014-09-04 03:21:00 +0200

    Bump to version 0.0.24

--- package.js
+++ package.js
@@ -1,15 +1,51 @@
 Package.describe({
-    summary: "Accounts Templates styled for Zurb Foundation."
+    summary: "Accounts Templates styled for Zurb Foundation.",
+    version: "0.0.24",
+    name: "splendido:accounts-templates-foundation",
+    git: "https://github.com/splendido/accounts-templates-foundation.git",
 });
 
 Package.on_use(function(api, where) {
-    api.use([
-        'service-configuration',
-        'accounts-base',
-        'accounts-templates-core',
-        'templating',
-        'less'
-    ], 'client');
+    if (api.versionsFrom) {
+        api.use([
+            'service-configuration@1.0.0',
+            'accounts-base@1.0.0',
+            'splendido:accounts-templates-core@0.0.24',
+            'templating@1.0.4',
+            'less@1.0.5'
+        ], 'client');
+        api.use([
+            'service-configuration@1.0.0',
+            'accounts-password@1.0.0',
+            'accounts-base@1.0.0',
+            'splendido:accounts-templates-core@0.0.24',
+        ], 'server');
+
+        api.imply([
+            'splendido:accounts-templates-core@0.0.24',
+            'service-configuration',
+        ], ['client', 'server']);
+    }
+    else{
+        api.use([
+            'service-configuration',
+            'accounts-base',
+            'accounts-templates-core',
+            'templating',
+            'less'
+        ], 'client');
+        api.use([
+            'service-configuration',
+            'accounts-password',
+            'accounts-base',
+            'accounts-templates-core',
+        ], 'server');
+
+        api.imply([
+            'accounts-templates-core',
+            'service-configuration',
+        ], ['client', 'server']);
+    }
 
     api.add_files([
         'lib/at_form.html',
@@ -21,25 +57,21 @@ Package.on_use(function(api, where) {
         'lib/full_page_at_form.html',
         'lib/at_foundation.less'
     ], ['client']);
-
-    api.use([
-        'service-configuration',
-        'accounts-password',
-        'accounts-base',
-        'accounts-templates-core',
-    ], 'server');
-
-    api.imply([
-        'accounts-templates-core',
-        'service-configuration',
-    ], ['client', 'server']);
 });
 
 Package.on_test(function(api) {
-    api.use([
-        'accounts-templates-foundation',
-        'accounts-templates-core',
-    ]);
+    if (api.versionsFrom) {
+        api.use([
+            'splendido:accounts-templates-foundation',
+            'splendido:accounts-templates-core@0.0.24',
+        ]);
+    }
+    else{
+        api.use([
+            'accounts-templates-foundation',
+            'accounts-templates-core',
+        ]);        
+    }
     api.use(['tinytest', 'test-helpers'], ['client', 'server']);
     api.add_files('tests/accounts-templates-foundation_tests.js', ['client', 'server']);
 });
