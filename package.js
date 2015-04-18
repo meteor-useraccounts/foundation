f039094eae44ce3e16e2fa8c7fa4fd6c03fe64b8
Luca Mussi <ing.luca.mussi@gmail.com>
2015-04-18 16:47:47 +0200

    patch release - Bump to version 1.9.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Accounts Templates styled for Zurb Foundation.',
-  version: '1.9.0',
+  version: '1.9.1',
   name: 'useraccounts:foundation',
   git: 'https://github.com/meteor-useraccounts/foundation.git',
 });
@@ -18,7 +18,7 @@ Package.on_use(function(api, where) {
   ], ['client', 'server']);
 
   api.imply([
-    'useraccounts:core@1.9.0',
+    'useraccounts:core@1.9.1',
   ], ['client', 'server']);
 
   api.add_files([
@@ -64,7 +64,7 @@ Package.on_use(function(api, where) {
 Package.on_test(function(api) {
   api.use([
     'useraccounts:foundation',
-    'useraccounts:core@1.9.0',
+    'useraccounts:core@1.9.1',
   ]);
 
   api.use([
